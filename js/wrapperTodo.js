bbb7719684fa26502c60619f6b0c74a09a7ad0a1
Nguyen <nguyenmaithaonguyen2222@gmail.com>
2024-03-13 16:12:14 +0700

    updates

---
+++ js/wrapperTodo.js
@@ -0,0 +1,50 @@
+/*
+<div class="wrapper-div">
+    <div class="div-1">
+        <div class="from">
+            `${fromDay}/${fromMonth}/${fromYear}`
+        </div>
+        <div class="div-1-2">
+            arrow
+        </div>
+        <div class="to">
+            `${toDay}/${toMonth}/${toYear}`
+        </div>
+    </div>
+    <div class="div-2">
+        <div class="div-2-1">
+            `${name}`
+        </div>
+    </div>
+    <div class="div-3">
+        <div class ="div-3-1">
+            View
+        </div>
+        <div class="div-3-2">
+            Delete
+        </div>
+    </div>
+</div>
+*/
+
+/*
+    Todo = 
+    {
+        id: 1,
+        name: "Do homework",
+        fromDay: 12,
+        fromMonth: 3,
+        fromYear: 2024,
+        toDay: 12, 
+        toMonth: 3,
+        toYear: 2024,
+        content: "",
+
+    }
+*/
+
+let wrapperTodo = document.getElementById('wrapperTodo');
+
+
+
+
